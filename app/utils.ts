3d02877b084077b0e4c411131d6cad29e71cc358
Yash Birla <birlayash730@gmail.com>
2024-01-16 23:43:13 +0530

    fix error on deployment

--- app/utils.ts
+++ app/utils.ts
@@ -1,19 +1,19 @@
 export function stringToColor(str: string) {
   let hash = 0;
   for (let i = 0; i < str.length; i++) {
-      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
   }
-  let color = '#';
+  let color = "#";
   for (let i = 0; i < 3; i++) {
-      let value = (hash >> (i * 8)) & 0xFF;
-      color += ('00' + value.toString(16)).substr(-2);
+    let value = (hash >> (i * 8)) & 0xff;
+    color += ("00" + value.toString(16)).substr(-2);
   }
   return color;
 }
 
 export function capitalizeFirstLetter(word: string) {
   if (!word) {
-    return '';
+    return "";
   }
   const firstLetter = word[0];
   const rest = word.slice(1, word.length);
@@ -22,22 +22,27 @@ export function capitalizeFirstLetter(word: string) {
 
 export function pickRandomItems<T>(arr: T[], numItems = 5) {
   let result = [];
-  for(let i = 0; i < numItems; i++) {
-      if(arr.length > 0) {
-          const randomIndex = Math.floor(Math.random() * arr.length);
-          result.push(arr[randomIndex]);
-          arr.splice(randomIndex, 1);
-      }
+  for (let i = 0; i < numItems; i++) {
+    if (arr.length > 0) {
+      const randomIndex = Math.floor(Math.random() * arr.length);
+      result.push(arr[randomIndex]);
+      arr.splice(randomIndex, 1);
+    }
   }
   return result;
 }
 
 export function parseJwt(token: string) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url?.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
 
   return JSON.parse(jsonPayload);
-};
+}
